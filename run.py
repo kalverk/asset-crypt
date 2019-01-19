876c8ce637f5ddf278700eb5725ef86b827faaaf
kalverk <kalverk@gmail.com>
2019-01-19 18:14:49 +0100

    Heroku run

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import os
 from src.app import create_app
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     env_name = os.getenv('FLASK_ENV') if os.getenv('FLASK_ENV') is not None else 'development'
     app = create_app(env_name)
-    app.run()
+    app.run(host='0.0.0.0', port=port)
